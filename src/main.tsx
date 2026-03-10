8bba2b887fecf894b4444dc934898572f6aaf1d3
uescobar <uescobar.360@gmail.com>
2026-03-10 16:54:23 -0600

    Generando proyecto

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <ChakraProvider value={defaultSystem}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ChakraProvider>,
+);
