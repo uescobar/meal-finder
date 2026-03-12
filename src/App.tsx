3932b9c0fb5ac51c78354cf416a6adfc181ae3d1
uescobar <uescobar.360@gmail.com>
2026-03-12 11:45:04 -0600

    buscando categrias

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,32 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import MainContent from "./components/MainContent";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  console.log("data", data);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    setLoading(true);
+
+    axios
+      .get(url, { signal })
+      .then(({ data }) => {
+        setData(data.meals);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+
   return (
     <Grid
       templateAreas={`"header header"
