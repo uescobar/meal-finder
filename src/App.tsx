c8895621da3b840831ffec9ce56068a1364713b9
uescobar <uescobar.360@gmail.com>
2026-03-12 11:24:00 -0600

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import Header from "./components/Header";
+import SideNav from "./components/SideNav";
+import MainContent from "./components/MainContent";
+
 function App() {
-  return <p>Hola Mundo</p>;
+  return (
+    <Grid
+      templateAreas={`"header header"
+                  "nav main"
+                  `}
+      gridTemplateRows={"60px 1fr"}
+      gridTemplateColumns={{ sm: `0 1fr`, md: `250px 1fr` }}
+    >
+      <GridItem pl="2" bg="orange.300" area={"header"}>
+        <Header />
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={"nav"} height="calc(100vh - 60px)">
+        <SideNav />
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        <MainContent />
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
