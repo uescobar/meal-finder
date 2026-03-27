589e570437d446bb5a046bbf91fa10dd32d203ec
uescobar <uescobar.360@gmail.com>
2026-03-26 18:06:28 -0600

    Dando estilo a las recetas

--- src/components/RecipeModalContent.tsx
+++ src/components/RecipeModalContent.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import type { Meal, MealDetails } from "../types";
+import { Dialog, Image, Text } from "@chakra-ui/react";
+import { Heading } from "lucide-react";
 
 type Props = {
   data: MealDetails;
 };
 
 function RecipeModalContent({ data }: Props) {
-  return <div>RecipeModalContent</div>;
+  return (
+    <>
+      <Dialog.Header>
+        <Dialog.Title>{data.strMeal}</Dialog.Title>
+      </Dialog.Header>
+      <Dialog.CloseTrigger />
+      <Dialog.Body>
+        <Image
+          width="100%"
+          src={data.strMealThumb}
+          alt={data.strMeal}
+          borderRadius="lg"
+        />
+        <Text mt="4" fontWeight="bold" fontSize="md" mb={4}>
+          Ingredientes
+        </Text>
+        <Text mt="4" whiteSpace="pre-line">
+          {data.strInstructions}
+        </Text>
+      </Dialog.Body>
+    </>
+  );
 }
 export default RecipeModalContent;
